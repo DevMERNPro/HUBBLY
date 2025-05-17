766022393de791dcb5984e6d3c45754ee647272d
Simran Apata <apatasimran@gmail.com>
2025-05-17 12:27:50 +0530

    Update config.js

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,5 +1,9 @@
  const config = {
-    BASE_URL : "http://localhost:8000/api/v1",
+    // BASE_URL : "http://localhost:8000/api/v1",
+      BASE_URL : "https://hubbly.onrender.com/api/v1",
+
+
+  
 }
 
 
